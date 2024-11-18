8ce3e2c7ab87359a168e0b4e17fdd392ecc5419d
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-11-18 20:47:10 +0100

    suppresion de limport de useEffect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState} from "react";
 import PropTypes from "prop-types";
 export default function Card({ card, updateCard }) {
   const [isOpen, setIsOpen] = useState(true);
